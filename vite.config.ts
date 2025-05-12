a6da8d17ce8f75ea1bf2e003fca3b5c98229d37f
Ivan Antipin <antipin-ia@10.211.53.24-vpn.dhcp.yndx.net>
2025-05-12 05:42:16 +0300

    Картинка для карточек

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/tg-shope/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
